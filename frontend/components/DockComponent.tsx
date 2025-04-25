b900c569667d26dd59d146419578e8d39145df5a
Marc Garcia <marcgaco@gmail.com>
2025-04-25 12:00:21 +0200

    Refactor event handling and improve UI components
    - Reorganized `getUserSubscribedEvents` to utilize a new `getAuth` function for better token management.
    - Enhanced `EventComponent` with improved error handling and UI updates for subscription status.
    - Updated `DockComponent` to dynamically set the active link based on the current route.
    - Improved styling and structure of event rendering for better user experience.

--- frontend/components/DockComponent.tsx
+++ frontend/components/DockComponent.tsx
@@ -1,56 +1,61 @@
-"use client"
+"use client";
 import { useUserContext } from "@/contexts/UserContext";
 import Dock from "@/src/blocks/Components/Dock/Dock";
-import { House, CalendarPlus2, Plus, UserRound } from 'lucide-react';
+import { House, CalendarPlus2, Plus, UserRound } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const DockComponent = () => {
   const router = useRouter();
   const { user } = useUserContext();
-  const [activeLink, setActiveLink] = useState<string>("Home");
+  let currentRouter = window.location.pathname.slice(1);
+  currentRouter =
+    currentRouter.charAt(0).toUpperCase() + currentRouter.slice(1);
+  const [activeLink, setActiveLink] = useState<string>(currentRouter);
 
   const items = [
-    { 
-      icon: <House color={activeLink === "Home" ? "black" : "white"} />, 
-      label: 'Home', 
+    {
+      icon: <House color={activeLink === "Home" ? "black" : "white"} />,
+      label: "Home",
       onClick: () => {
         setActiveLink("Home");
         router.push("/home");
       },
-      className: activeLink === "Home" ? "bg-lime-400" : ""
+      className: activeLink === "Home" ? "bg-lime-400" : "",
     },
-    { 
-      icon: <CalendarPlus2 color={activeLink === "Events" ? "black" : "white"} />, 
-      label: 'Events', 
+    {
+      icon: (
+        <CalendarPlus2 color={activeLink === "Events" ? "black" : "white"} />
+      ),
+      label: "Events",
       onClick: () => {
         setActiveLink("Events");
         router.push("/events");
       },
-      className: activeLink === "Events" ? "bg-lime-400" : ""
+      className: activeLink === "Events" ? "bg-lime-400" : "",
     },
-    { 
-      icon: <Plus color={activeLink === "Post" ? "black" : "white"} />, 
-      label: 'Post', 
+    {
+      icon: <Plus color={activeLink === "Post" ? "black" : "white"} />,
+      label: "Post",
       onClick: () => {
         setActiveLink("Post");
         router.push("/post");
       },
-      className: activeLink === "Post" ? "bg-lime-400" : ""
+      className: activeLink === "Post" ? "bg-lime-400" : "",
     },
-    { 
-      icon: <UserRound color={activeLink === "Profile" ? "black" : "white"} />, 
-      label: 'Profile', 
+    {
+      icon: <UserRound color={activeLink === "Profile" ? "black" : "white"} />,
+      label: "Profile",
       onClick: () => {
         setActiveLink("Profile");
         router.push(user?.user_name || "/404");
       },
-      className: activeLink === "Profile" ? "bg-lime-400" : ""
+      className: activeLink === "Profile" ? "bg-lime-400" : "",
     },
   ];
 
   return (
-    <Dock 
+    <Dock
       items={items}
       panelHeight={68}
       baseItemSize={50}
