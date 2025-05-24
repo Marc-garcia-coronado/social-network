97445e8d1bdce3c83b4cfb2612ad97df0370412a
Marc Garcia <marcgaco@gmail.com>
2025-05-24 14:28:23 +0200

    Update CORS configuration to allow requests from the production server URL

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -28,7 +28,7 @@ func (s *APIServer) Run() {
 	router := chi.NewRouter()
 	router.Use(chimw.Logger)
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:3001"},
+		AllowedOrigins:   []string{"http://social-network-production.up.railway.app"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		AllowCredentials: true,
