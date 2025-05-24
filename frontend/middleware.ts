9b5eabe88a896ac09227ab4329cc34924a7501ab
Marc Garcia <marcgaco@gmail.com>
2025-05-24 16:08:33 +0200

    Update middleware.ts

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,6 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
+  console.log(token);
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", request.url));
@@ -11,6 +12,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  // matcher: ["/:user_name/home","/:user_name/events","/:user_name/post","/:user_name/profile","/:user_name/settings"]
-  matcher: ["/:user_name/messages"]
+  matcher: ["/:user_name/:path*"],
 };
