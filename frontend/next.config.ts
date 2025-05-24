2825110b9553f6c9719aa4da16b4180d4f758d4f
Marc Garcia <marcgaco@gmail.com>
2025-05-24 16:41:50 +0200

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,22 +4,22 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        has: [
-          {
-            type: 'cookie',
-            key: 'token',
-            value: '(?<token>.*)',
-          },
-        ],
-        destination: '/login',
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       has: [
+  //         {
+  //           type: 'cookie',
+  //           key: 'token',
+  //           value: '(?<token>.*)',
+  //         },
+  //       ],
+  //       destination: '/login',
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
